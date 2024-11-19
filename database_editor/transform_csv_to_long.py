e7d898d074858e77e9868547d2f33fe404fa8c5b
ADonazzan <andrea.donazzan@studbocconi.it>
2024-11-19 17:22:51 +0100

    added logic to analyze different years

--- database_editor/transform_csv_to_long.py
+++ database_editor/transform_csv_to_long.py
@@ -7,17 +7,22 @@ df = pd.read_csv('../data/merged_data_original.csv')
 df.dropna(subset=['CIK_extracted'], inplace=True)
 
 # Melt the DataFrame to long format
-long_df = pd.melt(df, id_vars=['CIK_extracted', 'Ragione sociale', 'Codice NACE Rev. 2, core code (4 cifre)', 'Codice di consolidamento', 'Ultimo anno disp.'], var_name='variable', value_name='value')
+long_df = pd.melt(df, id_vars=['CIK_extracted', 'Ragione sociale', 'Codice NACE Rev. 2, core code (4 cifre)',
+                               'Codice di consolidamento', 'Ultimo anno disp.'], var_name='variable',
+                  value_name='value')
 
 # Extract the year and type of data from the variable column
 long_df['year'] = long_df['variable'].str.extract(r'(\d{4})')
-long_df['data_type'] = long_df['variable'].str.replace(r'\d{4}', '').str.strip()
+
+long_df['data_type'] = long_df['variable'].str.replace(r'\d{4}', '', regex=True).str.strip()
 
 # Drop the original variable column
 long_df.drop(columns=['variable'], inplace=True)
 
-# Pivot the DataFrame back to wide format
-wide_df = long_df.pivot_table(index=['CIK_extracted', 'Ragione sociale', 'Codice NACE Rev. 2, core code (4 cifre)', 'Codice di consolidamento', 'year'], columns='data_type', values='value', aggfunc='first').reset_index()
+# Pivot the DataFrame to have one column per variable type
+wide_df = long_df.pivot_table(
+    index=['CIK_extracted', 'Ragione sociale', 'Codice NACE Rev. 2, core code (4 cifre)', 'Codice di consolidamento',
+           'Ultimo anno disp.', 'year'], columns='data_type', values='value', aggfunc='first').reset_index()
 
 # Save the transformed DataFrame to a new CSV file
 wide_df.to_csv('../data/transformed_companies.csv', index=False)
--- main.py
+++ main.py
@@ -14,46 +14,65 @@ from datetime import datetime
 load_dotenv()
 
 
-def extract_data_companies(file_name: str):
+def extract_data_companies(file_name: str, excluded_companies_file_names: str = None):
     start_time = datetime.now()
     merged_companies_path = f'{os.getenv("BASE_PATH")}/data/{file_name}'
     df = pd.read_csv(merged_companies_path)
     # remove rows with missing CIK codes
     df = df[df['CIK_extracted'].notna()]
-    df.index = df['CIK_extracted']
-    df = df[~df.index.duplicated(keep='first')]
+
+    df['run_analysis'] = True
+    df['year_before'] = df['Ultimo anno disp.'] - 1
+
+    if excluded_companies_file_names:
+        excluded_companies = pd.read_csv(f'{os.getenv("BASE_PATH")}/data/{excluded_companies_file_names}')
+        excluded_companies = excluded_companies['cik_code'].tolist()
+        df.loc[df['CIK_extracted'].isin(excluded_companies) & (df['year'] == df['year_before']), 'run_analysis'] = False
+        df.drop(columns=['year_before'], inplace=True)
+
+    df.set_index(['CIK_extracted', 'year'], inplace=True)
     # Convert cik codes to string with no decimal points
-    cik_codes = df['CIK_extracted'].astype(int).astype(str).tolist()
-    # Extract date for each company and subtract 1 year
-    dates = df['Ultimo anno disp.'].tolist()
-    date_range = [(str(int(date) - 1), str(int(date) - 1)) for date in dates]
-    sentiment_results_all = {str(cik_code): {} for cik_code in cik_codes}
+    df.index = df.index.map(lambda x: (str(int(x[0])), x[1]))
+    df = df[~df.index.duplicated(keep='first')]
+
+    codes_years = df.index.tolist()
+    cik_codes = [code[0] for code in codes_years]
+    years = df.index.get_level_values('year').unique().tolist()
 
     chunks = [cik_codes[i:i + 200] for i in range(0, len(cik_codes), 200)]
     for j, chunk in enumerate(chunks):
-        sentiment_results = {str(cik_code): {} for cik_code in chunk}
+        sentiment_results = []
+        df = []
         for i, cik_code in enumerate(chunk):
             cik_code = str(cik_code)
-            date = date_range[i + j * 200]
-            extractor = TenKExtractor(cik_code, str(date[0]), str(date[1]))
-            ten_k_filings = extractor.get_ten_k_filings()
+            for year in years:
+                extractor = TenKExtractor(cik_code, str(year), str(year))
+                ten_k_filings = extractor.get_ten_k_filings()
 
-            for date, document in ten_k_filings.items():
-                logging.info(
-                    f"Analyzing document for company {i + j * 200}/{len(cik_codes) + 1}: {cik_code} on date {date}")
-                analyzer = SentimentAnalyzer()
-                features = analyzer.analyze_sections(document)
-                sentiment_results[cik_code] = features
+                for date, document in ten_k_filings.items():
+                    logging.info(
+                        f"Analyzing document for company {i + j * 200}/{len(cik_codes) + 1}: {cik_code} on date {date}")
+                    analyzer = SentimentAnalyzer()
+                    features = analyzer.analyze_sections(document)
+                    features['cik_code'] = cik_code
+                    features['year'] = year
+                    features['date'] = date
+                    sentiment_results.append(features)
 
-            if i % 5 == 0:
-                results_df = pd.DataFrame(sentiment_results.values(), index=sentiment_results.keys())
-                # save file with starting minute
-                results_df.to_csv(
-                    f'{os.getenv("BASE_PATH")}/data/sentiment_results/{file_name[:-4]}_{start_time.hour}_{start_time.minute}_{j}.csv')
+                if i % 5 == 0:
+                    results_df = pd.DataFrame(sentiment_results)
+                    # save file with starting minute
+                    results_df.to_csv(
+                        f'{os.getenv("BASE_PATH")}/data/sentiment_results/{file_name[:-4]}_{start_time.day}_'
+                        f'{start_time.hour}_{start_time.minute}_{j}.csv', index=False)
 
-        results_df = pd.DataFrame(sentiment_results.values(), index=sentiment_results.keys())
+        results_df = pd.DataFrame(sentiment_results)
         results_df.to_csv(
-            f'{os.getenv("BASE_PATH")}/data/sentiment_results/{file_name[:-4]}_{start_time.hour}_{start_time.minute}_{j}.csv')
+            f'{os.getenv("BASE_PATH")}/data/sentiment_results/{file_name[:-4]}_{start_time.day}_{start_time.hour}'
+            f'_{start_time.minute}_{j}.csv', index=False)
+
+        # Clear sentiment_results to free up memory
+        sentiment_results = []
 
     return sentiment_results
 
@@ -97,4 +116,4 @@ def main():
 
 
 if __name__ == '__main__':
-    extract_data_companies('merged_data.csv')
+    extract_data_companies('transformed_companies.csv', 'already_analyzed.csv')
